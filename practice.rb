b52fc0861ee31eae40ddf13a8d6e58207f82c5cc
Crisandry Javier <crisandryj@gmail.com>
2022-05-12 15:53:20 -0400

    palindrome

--- practice.rb
+++ practice.rb
@@ -7,4 +7,18 @@ def fact(n)
     end 
 end
 
-p fact(2)
+def collatz(n)
+    if (n == 1)
+        return 0 
+    elsif (n % 2 == 0)
+       return 1 + collatz(n/2)
+    else 
+      return 1 + collatz(3 * n + 1)
+    end 
+end 
+ 
+def palindrome(string)
+    
+
+end 
+
