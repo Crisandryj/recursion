bea090bd3167bab1b3b730d637ef7e99c7686fae
Crisandry Javier <crisandryj@gmail.com>
2022-05-15 11:51:09 -0400

    palidrome

--- practice.rb
+++ practice.rb
@@ -1,8 +1,20 @@
 def fact(n)
-if n == 0
-    return 1
-end 
-n * fact(n-1)
+    if n == 0
+        return 1
+    end 
+    n * fact(n-1)
 end
 
-p fact(7)
+def palindrome(string)
+    if string.length == 1 || string.length == 0
+        p "palindrome!"
+    else
+    if string[0] == string[-1]
+        palindrome(string[1..-2])
+    else
+        p "not a palindrome"
+    end
+    end
+end 
+
+palindrome("mada")
