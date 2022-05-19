e2332ad1ae953a0c3fb29771407e6dc7e8613e19
Crisandry Javier <crisandryj@gmail.com>
2022-05-19 13:34:31 -0400

    cleaned code

--- practice.rb
+++ practice.rb
@@ -79,35 +79,34 @@ def merge_sort(array)
   if array.size < 2
     array
   else
-    left = merge_sort(array[0...array.size/2])
-    right = merge_sort(array[array.size/2...array.size])
-    merge(left,right)
+    left = merge_sort(array[0...array.size / 2])
+    right = merge_sort(array[array.size / 2...array.size])
+    merge(left, right)
   end
 end
 
-def merge(left,right,array = [])
-   (left.size + right.size).times do
-    if  left.empty?
-        array << right.shift
+def merge(left, right, array = [])
+  (left.size + right.size).times do
+    if left.empty?
+      array << right.shift
     elsif right.empty?
-        array << left.shift
+      array << left.shift
     else
-       num = right  <=> left
-       if num == -1
-        array << right.shift
-       elsif num == 1
-        array << left.shift
-       else 
-        array << left.shift
-       end
+      num = right <=> left
+      array << if num == -1
+                 right.shift
+               elsif num == 1
+                 left.shift
+               else
+                 left.shift
+               end
     end
-end 
-    p array
+  end
+  p array
 end
 
 array = [4, 3, 2, 1]
 merge_sort(array)
-
 
 # if left[0] < right[0]
 #     a << right[0]
